f70f0ffab6cc318f2d3e2496b1869d18e0d6c738
Sever-ymy <ymy213456@163.com>
2023-04-23 20:43:22 +0800

    new file:   Ui_gui.py 	new file:   gui.ui 	new file:   main_ui.py 	new file:   qss/main.qss

---
+++ main_ui.py
@@ -0,0 +1,12 @@
+import PyQt6.QtCore as QtCore
+import PyQt6.QtGui as QtGui
+import PyQt6.QtWidgets as QtWidgets
+import sys
+
+class window(QtWidgets.QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setupUI()
+        
+    def setupUI(self):
+        pass
